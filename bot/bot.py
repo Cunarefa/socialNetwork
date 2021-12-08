c931aa3bc4b592e8f05ab1cc77eac7eb4dd2d962
Yevgen <yevgen.yelik@gmail.com>
2021-12-08 19:34:12 +0200

    make bot another way

--- bot/bot.py
+++ bot/bot.py
@@ -1,75 +1,78 @@
-import ast
 import random
+
 import requests
+from django.core.exceptions import ObjectDoesNotExist
 
-from conf import SIGNUP_URL, number_of_users, LOGIN_URL, POSTS_URL, max_posts_per_user, LIKES_URL, \
-    max_likes_per_user
-
+from conf import number_of_users, LOGIN_URL, SIGNUP_URL, max_posts_per_user, POSTS_URL, max_likes_per_user, LIKES_URL
 from faker import Faker
 
 fake = Faker()
 
 
-class DataGenerator:
+class Bot:
     def __init__(self):
-        self.headers = {}
         self.users = []
         self.posts = []
 
-    def registration(self):
+    def register(self):
         for _ in range(number_of_users):
             username = fake.unique.email()
             password = username
-            try:
-                requests.post(SIGNUP_URL, data={'username': username, 'password': password})
-            except Exception as err:
-                print(f'Error while creating new user with username {username}. Exception: {err}')
-                return {}
+            api = API(username, password)
+            user = api.send_request(SIGNUP_URL, data={'username': username, 'password': password})
+            if user:
+                user['password'] = password
+                self.users.append(user)
 
-            self.login(username, password)
-
-    def login(self, username, password):
-        login_data = {'username': username, 'password': password}
-        try:
-            response = requests.post(LOGIN_URL, data=login_data)
-            self.set_headers(response)
-        except Exception as err:
-            print(f"Exception while logining user with username {username} and password {password}. Exception: {err}")
-
-        self.create_random_posts()
-
-    def set_headers(self, response):
-        data_dict = ast.literal_eval(response.text)
-        token = data_dict['access']
-        self.headers = {'Authorization': f'Bearer {token}'}
-        self.users.append(self.headers)
-
-    def create_random_posts(self):
-        quantity_to_create = random.randint(1, max_posts_per_user)
-        for _ in range(quantity_to_create):
-            post_data = {'title': fake.text(max_nb_chars=20), 'content': fake.sentences(nb=5)}
-            try:
-                post = requests.post(POSTS_URL, data=post_data, headers=self.headers)
-            except Exception as err:
-                print(f"Exception while creating post with data: {post_data}. Exception: {err}")
-                return {}
-
-            self.posts.append(post)
+    def create_posts(self):
+        for user in self.users:
+            quantity_to_create = random.randint(1, max_posts_per_user)
+            api = API(user['username'], user['password'])
+            headers = api.get_headers()
+            for _ in range(quantity_to_create):
+                post_data = {'title': fake.text(max_nb_chars=20), 'content': fake.sentences(nb=5)}
+                post = api.send_request(POSTS_URL, data=post_data, headers=headers)
+                if post:
+                    self.posts.append(post)
 
     def set_likes(self):
-        for headers in self.users:
+        for user in self.users:
+            api = API(user['username'], user['password'])
+            headers = api.get_headers()
             for post in random.choices(self.posts, k=max_likes_per_user):
-                response_to_dict = ast.literal_eval(post.text)
-                post_id = response_to_dict['id']
-                data = {'post_id': post_id}
-                try:
-                    requests.post(LIKES_URL, headers=headers, data=data)
-                except Exception as err:
-                    print(f"Exception while liking post with id {post_id}. Exception: {err}")
-                    return {}
+                data = {'post_id': post['id']}
+                api.send_request(LIKES_URL, data=data, headers=headers)
+
+
+class API:
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+    def send_request(self, url, data, headers=None):
+        try:
+            response = requests.post(url=url, data=data, headers=headers)
+            return response.json()
+
+        except ValueError as err:
+            print(f"Wrong data input!. Exception: {err}")
+        except ObjectDoesNotExist as err:
+            print(f'User with that credentials does not exists. Exception: {err}')
+        except Exception as err:
+            print(f'Error while creating new user with username {self.username}. Exception: {err}')
+
+        return {}
+
+    def get_headers(self):
+        data = {'username': self.username, 'password': self.password}
+        response = requests.post(LOGIN_URL, data).json()
+        token = response['access_token']
+        headers = {'Authorization': f'Bearer {token}'}
+        return headers
 
 
 if __name__ == '__main__':
-    generator = DataGenerator()
-    generator.registration()
-    generator.set_likes()
+    bot = Bot()
+    bot.register()
+    bot.create_posts()
+    bot.set_likes()
